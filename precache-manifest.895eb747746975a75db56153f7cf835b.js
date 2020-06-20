89762a6fba89a4469c3ab1872ccda8f04e5f7c49
lordvacilaoerrocerto <lordvacilaoerrocerto@hotmail.com>
2020-06-20 11:21:11 -0300

    Updates

---
+++ precache-manifest.895eb747746975a75db56153f7cf835b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ae517bf47764e26af46d263ebbe6da0",
+    "url": "/lpr_app/index.html"
+  },
+  {
+    "revision": "a752f9fb05234ee39cb2",
+    "url": "/lpr_app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "660ab456751e748566f6",
+    "url": "/lpr_app/static/js/2.d49e4579.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lpr_app/static/js/2.d49e4579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a752f9fb05234ee39cb2",
+    "url": "/lpr_app/static/js/main.f4ab8ebb.chunk.js"
+  },
+  {
+    "revision": "9eace2c9b9554b78e315",
+    "url": "/lpr_app/static/js/runtime-main.6f2570c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lpr_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
